3c059d820bcb702f33b59fd29ddbc9fc2137f3af
Student <example@example.com>
2019-10-02 09:51:44 -0700

    morning commit, add racer.js

--- components/Racer.js
+++ components/Racer.js
@@ -1,6 +1,8 @@
 import React, {useState, useContext} from 'react';
+import {Text} from 'react-native';
 
 
 const Racer = ({name, age, sex}) = > {
-  
+  const [racers, setRacers] = useContext();
+  let RacerInfo =
 }
